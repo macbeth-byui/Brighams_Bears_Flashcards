2087d3a201c3f2074ca4cf48bc5faffc0cdcc140
macbeth-byui <macbethc@byui.edu>
2022-10-25 11:59:54 -0600

    Initial commit

---
+++ app/src/main/java/com/example/brighams_bears_flashcards/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.brighams_bears_flashcards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        println("Hello World")
+
+        println("hello")
+    }
+}
